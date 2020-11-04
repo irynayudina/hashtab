2bf43871c4f9666c63791b7a71c9f3d057c363b1
Iryna Yudina <yudinaira4444@gmail.com>
2020-11-04 16:48:44 +0200

    f

--- hashtab.cpp
+++ hashtab.cpp
@@ -3,19 +3,19 @@
 #include <iostream>
 using namespace std;
 // hash functions//////////////////////////////////////////////////////////////////////////////////////////////////
-int HashFunctionMultiplication(int k)
+int HashFunctionMultiplication(int k, int ts)
 {
-    int N = 33; double A = 0.618033;
+    int N = ts; double A = 0.618033;
     int h = floor(N * fmod(k * A, 1));
     return h;
 }
-int HashFunctionDivision(int k)
+int HashFunctionDivision(int k, int ts)
 {
-    return (k % 33);
+    return (k % ts);
 }
-int HashFunctionRowKeys(const char* ch)
+int HashFunctionRowKeys(const char* ch, int ts)
 {
-    int a = 29; int M = 33;
+    int a = 29; int M = ts;
     int result = 0;
     int size = sizeof(ch);
     for (int i = 0; i < size; i++) {
@@ -24,9 +24,9 @@ int HashFunctionRowKeys(const char* ch)
     result = result % M;
     return result;
 }
-int HashFunctionRowKeys(string ch)
+int HashFunctionRowKeys(string ch, int ts)
 {
-    int a = 26; int M = 13;
+    int a = 26; int M = ts;
     int result = 0;
     int size = ch.length();
     for (int i = 0; i < size; i++) {
@@ -55,7 +55,7 @@ ostream& operator<<(ostream& os, const hashTableEntryInt& dt)
     return os;
 }
 struct hashTableEntryString {
-    string e_key;
+    string e_key = "empty";
     int e_data;
     hashTableEntryString() {
          e_key = "empty";
@@ -83,123 +83,185 @@ private:
     int T_S;
     List<hashTableEntryString>* lists;
 public:
-    HashTable_open_hashing(int size = 33) {
+    HashTable_open_hashing(int size = 3) {
         T_S = size;
         lists = new List<hashTableEntryString>[T_S];
-        /*for (int i = 0; i < T_S; i++) {
-            lists[i] = NULL;
-        }*/
+        for (int i = 0; i < T_S; i++) {
+            //lists[i] = NULL;
+        }
+    }
+    int getTs() {
+        return T_S;
     }
     void insert_table_row(string k, hashTableEntryString d) {
-        lists[HashFunctionRowKeys(k)].createnode(d);
+        if (lists[HashFunctionRowKeys(k, T_S)].amount() == 0) {
+            //++count;
+        }
+        lists[HashFunctionRowKeys(k, T_S)].createnode(d);
     }
     void delete_table_row(string k) {
         node<hashTableEntryString>* temp = new node<hashTableEntryString>;
-        if (lists[HashFunctionRowKeys(k)].amount() != 0) {
-            temp = lists[HashFunctionRowKeys(k)].get_head();
+        if (lists[HashFunctionRowKeys(k, T_S)].amount() != 0) {
+            temp = lists[HashFunctionRowKeys(k, T_S)].get_head();
             int c = 0;
             while (temp != NULL)
             {
                 ++c;
                 if (temp->data.e_key == k) {
-                    lists[HashFunctionRowKeys(k)].delete_from_n_possition(c);
-                    //temp->data.e_key = "deleted";
-                    //temp->data.e_data = NULL;//or 0
+                    lists[HashFunctionRowKeys(k, T_S)].delete_from_n_possition(c);
                     break;
                 }
                 temp = temp->next;
             }
         }
-        
     }
     hashTableEntryString find_row(string k) {
         node<hashTableEntryString>* temp = new node<hashTableEntryString>;
-        if (lists[HashFunctionRowKeys(k)].amount() != 0) {
-            temp = lists[HashFunctionRowKeys(k)].get_head();
-            while (temp != NULL)
-            {
-                if (temp->data.e_key == k) {
-                    return temp->data;
-                    break;
+        int t = HashFunctionRowKeys(k, T_S);
+       // if ( t < T_S) { return; }
+        if (lists[t].amount() != 0) {
+            temp = lists[t].get_head();
+            if (temp->data.e_key != "empty"){
+                while (temp != NULL)
+                {
+                    if (temp->data.e_key == k) {
+                        return temp->data;
+                        break;
+                    }
+                    temp = temp->next;
                 }
-                temp = temp->next;
             }
+            
         }
         return temp->data;
     }
     void print_table() {
         for (int i = 0; i < T_S; i++) {
-            lists[i].display();
+                cout << i << ":: ";
+                lists[i].display();
         }
     }
     // hash functions//////////////////////////////////////////////////////////////////////////////////////////////////
-    friend int HashFunctionRowKeys(string ch);
+    friend int HashFunctionRowKeys(string ch, int T_S);
 };
 class HashTable_closed_hashing {
 private:
-    int T_S = 33;
+    int T_S = 3;
     hashTableEntryInt* table;
+    int count;
 public:
-    HashTable_closed_hashing(int size = 33) {
+    HashTable_closed_hashing(int size = 3) {
         T_S = size;
         table = new  hashTableEntryInt[T_S];
         for (int i = 0; i < T_S; i++) {
             table[i].e_key = -1;
         }
     }
+    int getTs() {
+        return T_S;
+    }
+    void resizeTable_division() {
+        if (static_cast<double>(count) / T_S > 0.8) {
+            hashTableEntryInt* tableTemp = new hashTableEntryInt[T_S];
+            for (int i = 0; i < T_S; i++) {;
+                    tableTemp[i].e_data = table[i].e_data;
+                    tableTemp[i].e_key = table[i].e_key;
+            }
+            T_S = T_S * 2;
+            delete table;
+            count = 0;
+            
+            table = new  hashTableEntryInt[T_S];
+            for (int i = 0; i < T_S; i++) {
+                table[i].e_key = -1;
+            }
+            for (int i = 0; i < T_S / 2; i++) {
+                //if (tableTemp[i].e_key != -1) {
+                    insert_table_division(tableTemp[i].e_key, tableTemp[i]);
+                //}                
+            }
+        }
+    }
+    void resizeTable_multiplication() {
+        if (static_cast<double>(count) / T_S > 0.8) {
+            hashTableEntryInt* tableTemp = new hashTableEntryInt[T_S];
+            for (int i = 0; i < T_S; i++) {
+                ;
+                tableTemp[i].e_data = table[i].e_data;
+                tableTemp[i].e_key = table[i].e_key;
+            }
+            T_S = T_S * 2;
+            delete table;
+            count = 0;
+
+            table = new  hashTableEntryInt[T_S];
+            for (int i = 0; i < T_S; i++) {
+                table[i].e_key = -1;
+            }
+            for (int i = 0; i < T_S / 2; i++) {
+                
+                insert_table_multiplication(tableTemp[i].e_key, tableTemp[i]);
+                               
+            }
+        }
+    }
     void insert_table_division(int k, hashTableEntryInt d) {
-        int formed_key = HashFunctionDivision(k);
+        int formed_key = HashFunctionDivision(k, T_S);
         for (int i = formed_key; i < T_S + formed_key; i++) {
             int j = i;
             (j >= T_S) ? j = i - T_S : j = i;
             if (table[j].e_key == -1)
             {
                 table[j] = d;
+                ++count;
                 break;
             }
         }
     }
     void insert_table_multiplication(int k, hashTableEntryInt d) {
-        int formed_key = HashFunctionMultiplication(k);
+        int formed_key = HashFunctionMultiplication(k, T_S);
         for (int i = formed_key; i < T_S + formed_key; i++) {
             int j = i;
             (j >= T_S) ? j = i - T_S : j = i;
             if (table[j].e_key == -1)
             {
                 table[j] = d;
+                ++count;
                 break;
             }
 
         }
     }
     void delete_table_division(int k) {
-        int formed_key = HashFunctionDivision(k);
+        int formed_key = HashFunctionDivision(k, T_S);
         for (int i = formed_key; i < T_S + formed_key; i++) {
             int j = i;
             (j >= T_S) ? j = i - T_S : j = i;
             if (table[j].e_key == k) {
                 table[j].e_key = -1;
                 table[j].e_data = NULL;
+                --count;
                 break;
             }
 
         }
     }
     void delete_table_multiplication(int k) {
-        int formed_key = HashFunctionMultiplication(k);
+        int formed_key = HashFunctionMultiplication(k, T_S);
         for (int i = formed_key; i < T_S + formed_key; i++) {
             int j = i;
             (j >= T_S) ? j = i - T_S : j = i;
             if (table[j].e_key == k) {
                 table[j].e_key = -1;
                 table[j].e_data = NULL;
+                --count;
                 break;
             }
 
         }
     }
     hashTableEntryInt find_division(int k) {
-        int formed_key = HashFunctionDivision(k);
+        int formed_key = HashFunctionDivision(k, T_S);
         for (int i = formed_key; i < T_S + formed_key; i++) {
             int j = i;
             (j >= T_S) ? j = i - T_S : j = i;
@@ -213,7 +275,7 @@ public:
         return h;
     }
     hashTableEntryInt find_multiplication(int k) {
-        int formed_key = HashFunctionMultiplication(k);
+        int formed_key = HashFunctionMultiplication(k, T_S);
         for (int i = formed_key; i < T_S + formed_key; i++) {
             int j = i;
             (j >= T_S) ? j = i - T_S : j = i;
@@ -228,14 +290,15 @@ public:
     }
     void print_table() {
         for (int i = 0; i < T_S; i++) {
-            if (table[i].e_key != -1) {
-                cout << table[i];
-            }            
+            //if (table[i].e_key != -1) {
+                cout <<  i << "::   ";
+                cout << table[i] <<endl;
+            //}            
         }
     }
     // hash functions//////////////////////////////////////////////////////////////////////////////////////////////////
-    friend int HashFunctionMultiplication(int k);
-    friend int HashFunctionDivision(int k);
+    friend int HashFunctionMultiplication(int k, int T_S);
+    friend int HashFunctionDivision(int k, int T_S);
 };
 
 
@@ -312,9 +375,11 @@ int main()
                     cin >> value;
                     hte_i_i.e_key = key; hte_i_i.e_data = value;
                     if (choise == 1) {
+                        hash_table_i_i_c.resizeTable_division();
                         hash_table_i_i_c.insert_table_division(hte_i_i.e_key, hte_i_i);
                     }
                     else if (choise == 2) {
+                        hash_table_i_i_c.resizeTable_multiplication();
                         hash_table_i_i_c.insert_table_multiplication(hte_i_i.e_key, hte_i_i);
                     }
                     break;
@@ -323,9 +388,11 @@ int main()
                     cin >> key;
                     if (choise == 1) {
                         hash_table_i_i_c.delete_table_division(key);
+                        hash_table_i_i_c.resizeTable_division();
                     }
                     else if (choise == 2) {
                         hash_table_i_i_c.delete_table_multiplication(key);
+                        hash_table_i_i_c.resizeTable_multiplication();
                     }
                     break;
                 case 3:
